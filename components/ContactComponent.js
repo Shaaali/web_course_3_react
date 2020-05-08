f1daedc114a165f6e126da1c844f014982bf87f1
Shalitha Pathiranage <shali96shri@gmail.com>
2020-05-08 16:08:37 +0530

    email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,8 +1,8 @@
 import React , { Component } from 'react';
 import { View , Text} from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card , Button , Icon} from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
-
+import * as MailComposer from 'expo-mail-composer';
 
 class ContactUs extends Component{
     constructor(props){
@@ -13,14 +13,22 @@ class ContactUs extends Component{
     }
    static navigationOptions = {
         title : 'Contact Us' 
-    };
+    }
+
+    sendMail(){
+        MailComposer.composeAsync({
+            recipients:['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern: '
+        })
+    }
 
     render(){
         return(
             <View>
                  <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
                     <Card title = "Contact Us">
-                        <Text>
+                        <Text style = {{margin : 10}}>
                         121, Clear Water Bay Road{"\n"}
                         Clear Water Bay, Kowloon{"\n"}
                         HONG KONG{"\n"}
@@ -28,6 +36,10 @@ class ContactUs extends Component{
                         Fax: +852 8765 4321 {"\n"}
                         Email:confusion@food.net
                         </Text>
+                        <Button
+                        title="Send Email" buttonStyle={{backgroundColor:'#512DA8'}}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white'/>}
+                        onPress={this.sendMail}></Button>        
                     </Card>
                  </Animatable.View>
                
