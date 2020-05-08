a9a403678b55a3ed564fbda8f2e51b245ad7aed6
Shalitha Pathiranage <shali96shri@gmail.com>
2020-05-08 14:23:07 +0530

    notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,7 +3,8 @@ import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Aler
 import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
-
+import * as Permissions from 'expo-permissions';
+import {Notifications} from 'expo';
 
 class Reservation extends Component {
     constructor(props){
@@ -44,7 +45,9 @@ class Reservation extends Component {
                 },
                 {
                     text: 'Reserve',
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();}
                 }
             ],
             { cancelable: false}
@@ -59,6 +62,37 @@ class Reservation extends Component {
             date: '',
             showModal: false
         });
+    }
+    
+    async obtainNotificationPermission(){
+        let permission= await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS)
+        if (permission.status!== 'granted'){
+            permission=await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status!=='granted'){
+                Alert.alert("Permission not granted to show notifications")
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date){
+        await this.obtainNotificationPermission();
+        
+        Notifications.createChannelAndroidAsync('Confusion', {
+            name: 'Confusion',
+            sound: true,
+            vibrate: true
+        })
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for ' + date + ' requested',
+            android:{
+                channelId: 'Confusion',
+                sound:true,
+                vibrate:true,
+                color:'#512DA8',
+            }
+        })
     }
 
     render(){
